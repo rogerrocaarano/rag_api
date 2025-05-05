1f129d75fa24e1858304921755c974044e92e535
Roger Roca Arano <git@rogerroca.me>
2025-05-05 12:09:25 -0400

    feat: add UserRegistrationController for Firebase user registration

---
+++ Presentation.RestApi/Controller/UserRegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.RestApi.Controller;
+
+[ApiController]
+[Authorize(AuthenticationSchemes = "Firebase")]
+[Route("[controller]")]
+public class UserRegistrationController(IFirebaseUsersRepository firebaseUsers) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> RegisterFirebaseUserIfNotExists()
+    {
+        var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            return Unauthorized("Invalid Firebase user.");
+        }
+
+        // add user to database if not exists
+        var userGuid = await firebaseUsers.GetUserByFirebaseId(userId) ?? 
+                       await firebaseUsers.AddFirebaseUser(userId);
+
+        return Ok($"User registered successfully with ID: {userGuid}");
+    }
+}
